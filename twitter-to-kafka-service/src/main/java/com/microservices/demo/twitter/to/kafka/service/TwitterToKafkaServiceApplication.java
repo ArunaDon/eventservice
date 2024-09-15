50b275e6672dc4926a89ee88ed48fca90d7da958
Dana <aruna@gmail.com>
2024-09-15 13:17:44 +0200

    init commit

--- twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -1,11 +1,23 @@
 package com.microservices.demo.twitter.to.kafka.service;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TwitterToKafkaServiceApplication {
+public class TwitterToKafkaServiceApplication implements CommandLineRunner {
     public static void main(String [] args){
         SpringApplication.run(TwitterToKafkaServiceApplication.class,args);
+    }
+
+    @PostConstruct
+    public void init(){
+
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Aplication starts....");
     }
 }
