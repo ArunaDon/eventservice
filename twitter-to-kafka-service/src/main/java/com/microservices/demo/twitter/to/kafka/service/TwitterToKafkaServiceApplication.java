c31a82aa3e4ce35ca70569ff52682892b9a0fa47
Dana <aruna@gmail.com>
2024-09-15 14:11:51 +0200

    adding log schemes

--- twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -1,12 +1,26 @@
 package com.microservices.demo.twitter.to.kafka.service;
 
+import com.microservices.demo.twitter.to.kafka.service.config.TwitterToKafkaServiceConfigData;
 import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class TwitterToKafkaServiceApplication implements CommandLineRunner {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TwitterToKafkaServiceApplication.class);
+
+    private final TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData ;
+
+ public TwitterToKafkaServiceApplication(TwitterToKafkaServiceConfigData configData){
+     this.twitterToKafkaServiceConfigData = configData;
+ }
+
     public static void main(String [] args){
         SpringApplication.run(TwitterToKafkaServiceApplication.class,args);
     }
@@ -18,6 +32,7 @@ public class TwitterToKafkaServiceApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Aplication starts....");
+        LOG.info("Aplication starts....");
+        LOG.info(Arrays.toString(twitterToKafkaServiceConfigData.getTwitterKeywords().toArray(new String[] {})));
     }
 }
