d83c1cc3e167cd31f62b09c573c6d7b968a1f2ba
Dana <aruna@gmail.com>
2024-10-06 16:02:14 +0200

    added config server basic auth

--- playground/src/main/java/jasypt/TestJasypt.java
+++ playground/src/main/java/jasypt/TestJasypt.java
@@ -6,10 +6,10 @@ import org.jasypt.iv.RandomIvGenerator;
 public class TestJasypt {
     public static void main(String [] args){
         StandardPBEStringEncryptor standardPBEStringEncryptor = new StandardPBEStringEncryptor();
-        standardPBEStringEncryptor.setPassword("Demo_Pwd!2020");
-        standardPBEStringEncryptor.setAlgorithm("PBEWithHmacSHA512AndAES_256");
+        standardPBEStringEncryptor.setPassword("");
+        standardPBEStringEncryptor.setAlgorithm("");
         standardPBEStringEncryptor.setIvGenerator(new RandomIvGenerator());
-        String result = standardPBEStringEncryptor.encrypt("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");
+        String result = standardPBEStringEncryptor.encrypt("");
         System.out.println(result);
         System.out.println(standardPBEStringEncryptor.decrypt(result));
     }
