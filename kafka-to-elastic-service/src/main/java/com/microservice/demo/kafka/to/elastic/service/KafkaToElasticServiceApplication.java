e63d372b181a5569b079225a13f1b9acf053f7a8
arunadon <nakandaladananjaya@gmail.com>
2024-10-16 10:36:59 +0200

    adding elastic cluster via docker compose:

--- kafka-to-elastic-service/src/main/java/com/microservice/demo/kafka/to/elastic/service/KafkaToElasticServiceApplication.java
+++ kafka-to-elastic-service/src/main/java/com/microservice/demo/kafka/to/elastic/service/KafkaToElasticServiceApplication.java
@@ -3,10 +3,9 @@ package com.microservice.demo.kafka.to.elastic.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.microservice.demo")
+@ComponentScan(basePackages = "com.microservice.demo,com.microservices.demo")
 public class KafkaToElasticServiceApplication {
     public static void main(String [] args){
         SpringApplication.run(KafkaToElasticServiceApplication.class,args);
